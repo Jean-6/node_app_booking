d7cb4fdbf9213df4c461982d194e51783c262800
Jean <oka.jeanjaures@gmail.com>
2023-01-26 01:03:01 +0100

    Normalisation du port avec normalizePort

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const http = require('http');
 const app = require("./app");
 
-app.set('port',process.env.PORT || 3000);
-const server = http.createServer(app);
-server.listen(process.env.PORT || 3000);
+const normalizePort = val=>{
 
-//const server = http.createServer(app);
-/*
+    const port=parseInt(val,10);
+    if(isNaN(port)){
+        return val;
+    }
+    if(port>=0){
+        return port;
+    }
+    return false;
+}
+const port=normalizePort(process.env.PORT || '3000');
+app.set('port',port);
 
- */
+
+
+
