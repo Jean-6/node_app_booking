982f309225ba8a93fe5d92a39a8ee2d96235d8d6
Jean <oka.jeanjaures@gmail.com>
2023-02-11 00:36:54 +0100

    add signup middleware with user existence check

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 
-
 router.post('/signup',userCtrl.signup);
 router.get('/all',userCtrl.getAllUser);
 router.delete('/delete/:id',userCtrl.deleteUser);
