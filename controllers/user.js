7e59dbc706907de026871548a9159e293d3b8512
Jean <oka.jeanjaures@gmail.com>
2023-02-11 00:22:33 +0100

    add signup middleware with user existence check

--- controllers/user.js
+++ controllers/user.js
@@ -1,19 +1,59 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const {hash} = require("bcrypt");
 
+//todo : display json message into postman after request launching
+//todo : signup with account checking  / connexion / disconnection
+//todo : send mail
+//todo : validate email | npm install express-validator
+
+exports.signup= (req, res, next) => {
+
+    User.findOne({email:req.body.email },function(err,user){
+        if(err){
+            console.log(err.message)
+        }
+        let  msg;
+        if(user){
+            msg="user exists"
+            console.log(msg)
+            res.json({message: msg})
+        }else{
+            bcrypt.hash(req.body.password,10)
+                .then(hash =>{
+                    const user= new User({
+                        name: req.body.name,
+                        firstname: req.body.firstname,
+                        email: req.body.email,
+                        login: req.body.login,
+                        password: hash
+                    });
+                    user.save()
+                        .then(()=>res.status(201).json({message: 'Ouverture d\'un compte utilisateur !'}))
+                        .catch(error => res.status(400).json({error}));
+                })
+                .catch(error => res.status(500).json({error}));
+        }
+    })
+}
+
+/*
+exports.login= async(req, res, next) => {
+    User.findOne({email:req.body.login})
+}
+*/
 exports.getAllUser= async(req, res, next) => {
     const users = await User.find()
         .then(users => res.status(200).json(users))
         .catch(error => res.status(501).json({error}));
 }
 
-//todo : display json message into postman after request launching
 exports.deleteUser=async(req, res, next) => {
     console.log("delete user");
     await User.deleteOne({id:req.params.id})
         .then(() => res.status(204).json({ message: 'user deleted !'}))
         .catch(error => res.status(501).json({ error }));
 }
-
 
 exports.createUser=async(req, res, next) => {
     console.log("create user");
@@ -57,4 +97,3 @@ exports.updatePartUserById=async(req, res) => {
         res.status(501).json({message : error.message});
     })
 }
-
