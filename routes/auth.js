471a3daaaa6a5789dc4498d919875ef61d42b6f8
Jean <oka.jeanjaures@gmail.com>
2023-02-16 09:52:57 +0100

    added authentication middleware and token model

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,7 @@ const router = express.Router();
 
 const authCtrl = require('../controllers/auth');
 
-
-/*router.post('/signup',authCtrl.signup);
+router.post('/signup',authCtrl.signup);
 router.post('/login',authCtrl.login);
-router.post('/logout',authCtrl.logout);*/
+router.post('/logout',authCtrl.logout);
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,9 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 
-router.post('/signup',userCtrl.signup);
-router.post('/login',userCtrl.login);
+
 router.get('/all',userCtrl.getAllUser);
 router.delete('/delete/:id',userCtrl.deleteUser);
-router.post('/add',userCtrl.createUser);
 router.get('/:id',userCtrl.getUserById);
 router.put('/update/:id',userCtrl.updateUserById);
 router.patch('/update-part/:id',userCtrl.updatePartUserById);
